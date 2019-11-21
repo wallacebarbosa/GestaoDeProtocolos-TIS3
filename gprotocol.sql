af9f2c5f1f9f5dd49d2c6551cfe361655414cfe6
Andre Murilo <35697459+andre-murilo@users.noreply.github.com>
2019-11-21 09:40:02 -0300

    Add files via upload

--- gprotocol.sql
+++ gprotocol.sql
@@ -1,151 +1,174 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 21-Nov-2019 às 12:39
+-- Versão do servidor: 5.7.23
+-- versão do PHP: 7.2.10
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 100140
- Source Host           : localhost:3306
- Source Schema         : gprotocol
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 100140
- File Encoding         : 65001
 
- Date: 20/11/2019 01:08:09
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `gprotocol`
+--
 
--- ----------------------------
--- Table structure for anexo
--- ----------------------------
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `anexo`
+--
+
 DROP TABLE IF EXISTS `anexo`;
-CREATE TABLE `anexo`  (
+CREATE TABLE IF NOT EXISTS `anexo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `protocolo_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `date` datetime(0) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Table structure for comentario
--- ----------------------------
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentario`
+--
+
 DROP TABLE IF EXISTS `comentario`;
-CREATE TABLE `comentario`  (
+CREATE TABLE IF NOT EXISTS `comentario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `protocolo_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `comentario` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `data` datetime(0) DEFAULT NULL,
+  `comentario` varchar(255) DEFAULT NULL,
+  `data` datetime DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Table structure for encaminhamento
--- ----------------------------
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `encaminhamento`
+--
+
 DROP TABLE IF EXISTS `encaminhamento`;
-CREATE TABLE `encaminhamento`  (
+CREATE TABLE IF NOT EXISTS `encaminhamento` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `protocolo_id` int(11) DEFAULT NULL,
   `remetente_id` int(11) DEFAULT NULL,
-  `dest_tipo` enum('SETOR','USUARIO') CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT 'SETOR',
+  `dest_tipo` enum('SETOR','USUARIO') DEFAULT 'SETOR',
   `destinatario_id` int(11) DEFAULT NULL,
-  `tipo` enum('ENCAMINHAR','REJEITAR','ACEITAR','CRIAR') CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT 'CRIAR',
-  `data` datetime(0) DEFAULT NULL,
-  `justificativa` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `tipo` enum('ENCAMINHAR','REJEITAR','ACEITAR','CRIAR') DEFAULT 'CRIAR',
+  `data` datetime DEFAULT NULL,
+  `justificativa` text,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of encaminhamento
--- ----------------------------
-INSERT INTO `encaminhamento` VALUES (1, 5, 1, 'SETOR', 4, 'CRIAR', '2019-10-23 12:00:00', '');
-INSERT INTO `encaminhamento` VALUES (2, 5, 1, 'SETOR', 1, 'ACEITAR', '2019-10-30 08:00:00', NULL);
-INSERT INTO `encaminhamento` VALUES (3, 5, 1, 'SETOR', 1, 'REJEITAR', '2019-10-30 00:00:00', NULL);
-INSERT INTO `encaminhamento` VALUES (14, 6, 1, 'SETOR', 1, 'REJEITAR', '2019-11-20 00:56:35', 'Houve uma falha no engano. Ops!');
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for protocolo
--- ----------------------------
+--
+-- Estrutura da tabela `protocolo`
+--
+
 DROP TABLE IF EXISTS `protocolo`;
-CREATE TABLE `protocolo`  (
+CREATE TABLE IF NOT EXISTS `protocolo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `dataCriacao` datetime(0) DEFAULT NULL,
+  `titulo` varchar(64) NOT NULL,
+  `dataCriacao` datetime DEFAULT NULL,
   `usuario_id` int(11) DEFAULT NULL,
   `setor_id` int(11) NOT NULL,
-  `descricao` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of protocolo
--- ----------------------------
-INSERT INTO `protocolo` VALUES (5, 'Entrega de vacinas', '2019-10-23 15:20:00', 1, 1, 'Aviso de entrega das vacinas para o dia 25/10/2019.');
-INSERT INTO `protocolo` VALUES (6, 'Entrega de documentos', '2019-11-06 18:30:00', 1, 1, 'Envio de documentos para a unidade Santa Luzia.');
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for setor
--- ----------------------------
+--
+-- Estrutura da tabela `setor`
+--
+
 DROP TABLE IF EXISTS `setor`;
-CREATE TABLE `setor`  (
+CREATE TABLE IF NOT EXISTS `setor` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `nome` varchar(64) NOT NULL,
   `unidade_id` int(11) NOT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of setor
--- ----------------------------
-INSERT INTO `setor` VALUES (1, 'Rouparia', 1);
-INSERT INTO `setor` VALUES (2, 'Rouparia', 2);
-INSERT INTO `setor` VALUES (3, 'Rouparia', 3);
-INSERT INTO `setor` VALUES (4, 'Vacinacao', 1);
-INSERT INTO `setor` VALUES (5, 'Vacinacao', 2);
-INSERT INTO `setor` VALUES (6, 'Vacinacao', 3);
-INSERT INTO `setor` VALUES (7, 'Alta Gerencia', 1);
-INSERT INTO `setor` VALUES (8, 'Alta Gerencia', 2);
-INSERT INTO `setor` VALUES (9, 'Alta Gerencia', 3);
+--
+-- Extraindo dados da tabela `setor`
+--
 
--- ----------------------------
--- Table structure for unidade
--- ----------------------------
+INSERT INTO `setor` (`id`, `nome`, `unidade_id`, `endereco`) VALUES
+(1, 'Rouparia', 1, 'Concordia, Rua Itamacaraci 535'),
+(2, 'Rouparia', 2, 'Concordia, Rua Itamacaraci 535'),
+(3, 'Rouparia', 3, 'Concordia, Rua Itamacaraci 535'),
+(4, 'Vacinacao', 1, 'Concordia, Rua Itamacaraci 535'),
+(5, 'Vacinacao', 2, 'Concordia, Rua Itamacaraci 535'),
+(6, 'Vacinacao', 3, 'Concordia, Rua Itamacaraci 535'),
+(7, 'Alta Gerencia', 1, 'Concordia, Rua Itamacaraci 535'),
+(8, 'Alta Gerencia', 2, 'Concordia, Rua Itamacaraci 535'),
+(9, 'Alta Gerencia', 3, 'Concordia, Rua Itamacaraci 535');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `unidade`
+--
+
 DROP TABLE IF EXISTS `unidade`;
-CREATE TABLE `unidade`  (
+CREATE TABLE IF NOT EXISTS `unidade` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `nome` varchar(64) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of unidade
--- ----------------------------
-INSERT INTO `unidade` VALUES (1, 'Unidade Concordia');
-INSERT INTO `unidade` VALUES (2, 'Unidade Santa Luzia');
-INSERT INTO `unidade` VALUES (3, 'Unidade BH');
+--
+-- Extraindo dados da tabela `unidade`
+--
 
--- ----------------------------
--- Table structure for usuario
--- ----------------------------
+INSERT INTO `unidade` (`id`, `nome`) VALUES
+(1, 'Unidade Concordia'),
+(2, 'Unidade Santa Luzia'),
+(3, 'Unidade BH');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
 DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE `usuario`  (
+CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `senha` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `telefone` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `cargo` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `setor_id` int(11) NOT NULL DEFAULT 0,
+  `nome` varchar(64) NOT NULL,
+  `senha` varchar(64) NOT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `telefone` varchar(16) DEFAULT NULL,
+  `cargo` varchar(32) NOT NULL,
+  `setor_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of usuario
--- ----------------------------
-INSERT INTO `usuario` VALUES (1, 'admin', '1234', 'admin@gprotocol.com', '(31) 9 9999-9999', 'Administrador', 1);
+--
+-- Extraindo dados da tabela `usuario`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `usuario` (`id`, `nome`, `senha`, `email`, `telefone`, `cargo`, `setor_id`) VALUES
+(1, 'admin', '1234', 'admin@gprotocol.com', '(31) 9 9999-9999', 'Administrador', 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
